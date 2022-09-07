0c2fe5617c86fa3fe4eb8b13dd36948819db0ce0
Maria Pyrina <mpyrina@cfc.ethz.ch>
2022-09-07 17:24:39 +0200

    function fix

--- L_functions.py
+++ L_functions.py
@@ -5,15 +5,23 @@ from datetime import timedelta
 from datetime import date
 import time
 
-def sel_train_data_lead(nc_in_files,var_name,dim_to_stack,target_len,
+import numpy as np
+import xarray as xr
+from datetime import datetime 
+from datetime import timedelta
+from datetime import date
+import time
+
+import pandas as pd
+
+def sel_train_data_lead(nc_in_file,target_len,
                         s_target_date,e_target_date,
                         rw_1,lead_time,rw,ntimestep):
     '''
-    This function inputs multiple nc files with the same dimension 
-    (e.g., nc_in_files=path+'PC_serie*.nc') and reads them as a xarray. From these files
-    a predictor array will be created. The dimension to stack the files must be given(e.g.,
-    dim_to_stack='mode') and the variable name of the files (e.g., var_name='pcs').
-    The length of the target must be given (target_len).
+    This function inputs a 2-D file.nc, reads it as a xarray and creates
+    a predictor array. 1_D:time, 2_D:features
+    
+    The length of the target time series must be given (target_len).
     The start date and end date that we want to predict must be given 
     (e.g., s_target_date='16-10-1980', e_target_date='16-12-2021') and
     the running window that was already applied on the predictors with center=False must be
@@ -21,9 +29,12 @@ def sel_train_data_lead(nc_in_files,var_name,dim_to_stack,target_len,
     
     The predictor is selected in a way so that the 
     needed date is predicted at a certain lead time (lead_time) and for a specific running
-    window that was applied on the target (rw). Moreover, a selected time step for the LSTM 
+    window that was applied on the target with center=True (rw). If center=False, then set rw=0.
+    Moreover, a selected time step for the LSTM 
     is considered (ntimestep).
     '''
+    
+    print('starting')
 
     SDD = int(s_target_date[0:2])
     SMM = int(s_target_date[3:5])
@@ -36,13 +47,14 @@ def sel_train_data_lead(nc_in_files,var_name,dim_to_stack,target_len,
     print('end target',EDD,EMM,EYY)
 
     half_rw = int(rw/2)
-
-    # Import predictors if the predictors have been saved before
-    pc_xr = xr.open_mfdataset(root_results+nc_in_files,
-                              concat_dim=dim_to_stack,combine="nested")# 
-
+    
+    # Create correctly formated datetime
     date_target = datetime.strftime(datetime(year=SYY,month=SMM,day=SDD), "%Y.%m.%d")
-    pc_predictor = np.ndarray((target_len,ntimestep,int(pc_xr[dim_to_stack].shape[0])))
+    
+    # Initialize shape of the final predictor array
+    
+    pc_predictor = [] # np.ndarray((target_len,ntimestep,int(nc_in_file[var_name].shape[1])))
+    time_list = []
     it = 0
     ii = 0
     YYY = SYY
@@ -51,7 +63,13 @@ def sel_train_data_lead(nc_in_files,var_name,dim_to_stack,target_len,
         date_end = datetime.strftime(datetime.strptime(date_target, "%Y.%m.%d")-    timedelta(days=half_rw+lead_time+rw_1),"%Y.%m.%d")
         #print(date_target,date_start,date_end,it)
         #pc_predictor[ii,:,:] = pc_xr.sel(time = slice(date_start,date_end)).values
-        pc_predictor[ii,:,:] = pc_xr.sel(time = slice(date_start,date_end))[var_name].values
+        #print(nc_in_file)
+        f = nc_in_file.sel(time = slice(date_start,date_end))
+        f=f.assign_coords(time=range(ntimestep))
+        time_list.append(date_target)
+        #print(date_target)
+        
+        pc_predictor.append(f)
         if date_target == datetime.strftime(datetime(year=YYY,month=EMM,day=EDD),"%Y.%m.%d"):
             YYY = YYY+1
             date_target = datetime.strftime(datetime(year=YYY,month=SMM,day=SDD), "%Y.%m.%d")
@@ -61,7 +79,9 @@ def sel_train_data_lead(nc_in_files,var_name,dim_to_stack,target_len,
             it = 1
         ii = ii+1
         date_target = datetime.strftime(datetime.strptime(date_target, "%Y.%m.%d")+timedelta(days=it),"%Y.%m.%d") 
-
-    print('pc_predictor_shape',pc_predictor.shape)
+    pc_predictor = xr.concat(pc_predictor,"new_time").rename({"time":"lag"}).rename({"new_time":"time"})
+    pc_predictor = pc_predictor.assign_coords(time=time_list)
+    pc_predictor = pc_predictor.assign_coords(time=pd.DatetimeIndex(pc_predictor.time)) #-pd.Timedelta("15 d"))
+    #print('pc_predictor_shape',pc_predictor.shape)
     return pc_predictor
     
