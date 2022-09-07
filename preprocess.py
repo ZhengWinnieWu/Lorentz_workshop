5e081e55d35ae92aa4a71958d83408b2ba403df1
Aaron Spring <aaron.spring@mpimet.mpg.de>
2022-09-07 08:12:23 +0000

    fix

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,22 @@
 import xarray as xr
 import numpy as np
 
-def get_principle_components_and_EOFs(var_anom_sel, nmode=5, eofscaling=2, pcscaling=1):
-    """Returns Principle Components (PC) xr.DataArray and print how much total variance is explained by nmode modes."""
-    # need to use main branch
-    # b
+def get_principle_components_and_EOFs(da, nmode=5, eofscaling=2, pcscaling=1):
+    """Returns Principle Components (PC) and EOFs as xr.DataArray and print how much total variance is explained by nmode modes from an input xr.DataArray."""
+    # need to use main eofs branch
+    # pip install git+https://github.com/ajdawson/eofs.git
     # todo: xeofs from Niclas
     from eofs.xarray import Eof
-    lon2d,lat2d = np.meshgrid(var_anom_sel.lon,var_anom_sel.lat)
+    assert "lon" in da.coords, "expects dimension lon and lat"
+    assert "lat" in da.coords, "expects dimension lon and lat"
+    
+    lon2d,lat2d = np.meshgrid(da.lon, da.lat)
     wgts = np.cos(lat2d/180*np.pi)**0.5
-    solver = Eof(var_anom_sel, weights=wgts)
+    solver = Eof(da, weights=wgts)
 
     EOF = solver.eofs(neofs=nmode, eofscaling=eofscaling)
-    # EOF.plot(col="mode")
-    # eigenv = solver.eigenvalues(neigs=nmode)
     VarEx = solver.varianceFraction(neigs=nmode) * 100
-    print(f'the first {nmode} modes explain {round(sum(VarEx.values),2)}% of the total variance of {var_anom_sel.name}')
+    print(f'the first {nmode} modes explain {round(sum(VarEx.values),2)}% of the total variance of {da.name}')
     
     PC = solver.pcs(npcs=nmode, pcscaling=pcscaling)
     return PC, EOF
